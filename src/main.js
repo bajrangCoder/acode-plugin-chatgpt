f3d74089de1aa6a912a6181787d3dde0e9635697
Raunak Raj <nkray21111983@gmail.com>
2023-05-22 13:45:21 +0530

    Test.....

--- src/main.js
+++ src/main.js
@@ -138,6 +138,9 @@ class Chatgpt {
   }
 
   async generateImage() {
+    /*
+    for Generating image
+    */
     try {
       if(!this.$promtArea.value) {
         acode.alert("Warning", "Prompt is required");
@@ -233,16 +236,27 @@ class Chatgpt {
         typographer: false,
         quotes: '“”‘’',
         highlight: function(str, lang) {
-          let html = `<pre class="hljs codeBlock"><code>${hljs.highlightAuto(str).value}</code></pre>`;
-          return html;
+          const copyBtn = `<button class="copy-button"><svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em"><path fill="currentColor" d="M15 37.95q-1.25 0-2.125-.875T12 34.95v-28q0-1.25.875-2.125T15 3.95h22q1.25 0 2.125.875T40 6.95v28q0 1.25-.875 2.125T37 37.95Zm0-3h22v-28H15v28Zm-6 9q-1.25 0-2.125-.875T6 40.95V12.3q0-.65.425-1.075Q6.85 10.8 7.5 10.8q.65 0 1.075.425Q9 11.65 9 12.3v28.65h22.2q.65 0 1.075.425.425.425.425 1.075 0 .65-.425 1.075-.425.425-1.075.425Zm6-37v28-28Z"/></svg></button>`;
+          const codesArea = `<pre class="hljs codesArea"><code>${hljs.highlightAuto(str).value}</code></pre>`;
+          const codeBlock = `<div class="codeBlock">${copyBtn}${codesArea}</div>`;
+          copyBtn.onclick = this._copyCodeUtility.bind(this);
+          /*document.querySelector(".copy-button").addEventListener("click", (e) => {
+            window.alert("hi")
+          })*/
+          return codeBlock;
         }
       });
+      
       this.$sendBtn.addEventListener("click", this.sendQuery.bind(this))
 
       this.$page.show();
     } catch(error) {
       window.alert(error);
     }
+  }
+  
+  async _copyCodeUtility(){
+    window.alert("hi")
   }
 
   async updateApiToken() {
